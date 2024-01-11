bcf39bd554a23a9f686aa9d5d9a31577eaf4f3ef
Lương Tien <luongtienthanh2k2@gmail.com>
2024-01-11 20:48:00 +0700

    new bao tin phat

---
+++ js/sidebar.js
@@ -0,0 +1,32 @@
+$(function() {
+  
+  transition_timeout = 40;
+  $('.title_items').toggleClass('active');
+  current = $('.title_items').next().find('li');
+  current.toggleClass('visible');
+  for( i = 0; i <= current.length; i++ ) {
+        $(current[i]).css('transition-delay', transition_timeout * i + 'ms');
+      }
+
+
+
+  $('.title_items').click(function() {
+    
+    current = $(this).next().find('li');
+    
+    $(this).toggleClass('active');
+    current.toggleClass('visible');
+    
+    if ($(this).hasClass('active')) {
+      for( i = 0; i <= current.length; i++ ) {
+        $(current[i]).css('transition-delay', transition_timeout * i + 'ms');
+      }
+    }
+    else {
+      for( i = current.length, j = -1; i >= 0; i--, j++) {
+        $(current[i]).css('transition-delay', transition_timeout * j + 'ms');
+      }
+    }
+  
+  });
+});
---
+++ js/zoom_image.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function () {
+    $(".block__pic").imagezoomsl({
+        zoomrange: [1, 1]
+    });
+});
